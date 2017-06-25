aec95d3d363e7407f744a26ce7f8c4bbb42f779e
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2017-06-25 16:35:17 +0900

    Remove unnecessary comments

--- train_ft.py
+++ train_ft.py
@@ -37,18 +37,14 @@ class DelGradient(object):
                         grad *= 0
 
 def copy_model(src, dst):
-    # assert isinstance(src, link.Chain)
-    # assert isinstance(dst, link.Chain)
     assert isinstance(src, chainer.Chain)
     assert isinstance(dst, chainer.Chain)
     for child in src.children():
         if child.name not in dst.__dict__: continue
         dst_child = dst[child.name]
         if type(child) != type(dst_child): continue
-        # if isinstance(child, link.Chain):
         if isinstance(child, chainer.Chain):
             copy_model(child, dst_child)
-        # if isinstance(child, link.Link):
         if isinstance(child, chainer.Link):
             match = True
             for a, b in zip(child.namedparams(), dst_child.namedparams()):
